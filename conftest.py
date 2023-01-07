5a631d916bdfc1d46b3a4f863194bdab829e4ef2
svasilchenko <liketravelin@gmail.com>
2023-01-07 22:04:21 +0300

    add tests and conftest

---
+++ conftest.py
@@ -0,0 +1,58 @@
+import csv
+import os
+import pathlib
+import zipfile
+from io import TextIOWrapper, BytesIO
+from openpyxl import load_workbook
+import PyPDF2
+
+import pytest
+
+path_zip_arch = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources/files.zip')
+
+
+@pytest.fixture(scope='session', autouse=True)
+def zip_packer():
+    files_dir = pathlib.Path("resources/")
+    current_dir = pathlib.Path("")
+    with zipfile.ZipFile("files.zip", mode="w") as archive:
+        for file_path in files_dir.iterdir():
+            archive.write(file_path, arcname=file_path.name)
+        pathlib.Path(f'{current_dir}/files.zip').rename(f'{files_dir}/files.zip')
+    yield
+    pathlib.Path(f'{files_dir}/files.zip').unlink()
+
+
+@pytest.fixture
+def get_list_filenames():
+    """Фикстура для получения списка имен файлов в zip архиве"""
+    with zipfile.ZipFile(f"{path_zip_arch}", mode="r") as archive:
+        return archive.namelist()
+
+
+@pytest.fixture
+def get_csv_addresses():
+    """Фикстура для получения csv reader с файлом addresses.csv"""
+    with zipfile.ZipFile(f"{path_zip_arch}", mode="r") as archive:
+        with archive.open('addresses.csv', 'r') as infile:
+            reader = csv.reader(TextIOWrapper(infile, 'utf-8'))
+            return list(reader)
+
+
+@pytest.fixture
+def get_pdf_reader():
+    """Фикстура для получения pdf reader с файлом addresses.csv"""
+    with zipfile.ZipFile(f"{path_zip_arch}", mode="r") as archive:
+        filename = archive.namelist()[2]
+        pdf_file = PyPDF2.PdfReader(BytesIO(archive.read(filename)))
+        return pdf_file
+
+
+@pytest.fixture
+def get_xlsx_reader():
+    """Фикстура для получения xlsx reader с файлом addresses.csv"""
+    with zipfile.ZipFile(f"{path_zip_arch}", mode="r") as archive:
+        filename = archive.namelist()[1]
+        workbook = load_workbook(BytesIO(archive.read(filename)))
+        sheet = workbook.active
+        return sheet
---
+++ tests/test_files_from_zip.py
@@ -0,0 +1,29 @@
+# from zip_packer import zip_packer
+
+# zip_packer()
+
+
+def test_namelist_files(get_list_filenames):
+    expected_filenames = ['addresses.csv', 'file_example_XLSX_10.xlsx', 'sample.pdf']
+    assert len(expected_filenames) == len(get_list_filenames)
+    assert all([a == b for a, b in zip(expected_filenames, get_list_filenames)])
+
+
+def test_csv(get_csv_addresses):
+    list_adresses = [['John', 'Doe', '120 jefferson st.', 'Riverside', ' NJ', ' 08075'],
+                     ['Jack', 'McGinnis', '220 hobo Av.', 'Phila', ' PA', '09119'],
+                     ['John "Da Man"', 'Repici', '120 Jefferson St.', 'Riverside', ' NJ', '08075'],
+                     ['Stephen', 'Tyler', '7452 Terrace "At the Plaza" road', 'SomeTown', 'SD', ' 91234'],
+                     ['', 'Blankman', '', 'SomeTown', ' SD', ' 00298'],
+                     ['Joan "the bone", Anne', 'Jet', '9th, at Terrace plc', 'Desert City', 'CO', '00123']]
+    assert len(list_adresses) == len(get_csv_addresses)
+    assert all([a == b for a, b in zip(list_adresses, get_csv_addresses)])
+
+
+def test_pdf(get_pdf_reader):
+    assert 2 == len(get_pdf_reader.pages)
+    assert "This is a small demonstration .pdf file " in get_pdf_reader.pages[0].extract_text()
+
+
+def test_xlsx(get_xlsx_reader):
+    assert 'Mara' == get_xlsx_reader.cell(row=3, column=2).value
